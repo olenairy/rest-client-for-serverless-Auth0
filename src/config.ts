8db52b7485cc3ea7ecca9559b12c74e63771a6b2
Olena Chudnovets <olenairy@gmail.com>
2021-02-25 16:20:02 -0400

    First commit

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const apiEndpoint = process.env.API_ENDPOINT
+
+export const authConfig = {
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    callbackUrl: process.env.AUTH0_CALLBACK
+  }
+
+ 
